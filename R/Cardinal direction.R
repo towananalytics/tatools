9ec5cb4f2358d71bffe02816bfa22a8843a48b75
Nick Dawe <nick@towananalytics.comgit config --global user.name Nick>
2021-02-17 23:37:15 +0800

    Added abbreviation arg

--- R/Cardinal direction.R
+++ R/Cardinal direction.R
@@ -2,9 +2,10 @@
 #'
 #' This function inserts a new variable "cardinal.dir" into the dataframe passed to it..
 #'
-#' @param data a dataframe containing the data
-#' @param !!dir_col direction column to convert to cardinal factor.
-#' @return the original dataframe with the cardinal varaible included
+#' @param data a dataframe containing the data.
+#' @param dir_col direction column to convert to cardinal factor.
+#' @param abbr TRUE/FALSE should the results be abbreviated (N versus North)
+#' @return the original dataframe with the cardinal variable included
 #' @import dplyr
 #' @examples
 #'
@@ -16,47 +17,80 @@
 
 #' @export
 
-insert_cardinal_dir <- function(data, dir_col) {
+insert_cardinal_dir <- function(data, dir_col, abbr = TRUE) {
     # Check that data is in decimal degress:
    #if (data[, deparse(substitute(dir_col))] > 360) stop("Directions are not in degrees", FALSE)
 
     dat <- data
     dir_col <- enquo(dir_col)
     deparse(substitute(dir_col)) # for labelling
-    # look up this tutorial to insert data from dplyr:
-    # https://medium.com/optima-blog/writing-your-own-dplyr-functions-a1568720db0d
-# dat %>% select(.dots = lazyeval::lazy_dots(!!dir_col)) %>%
-    dat <- dat %>%
-           mutate(cardinal.dir = factor(ifelse(!!dir_col >= 0 & !!dir_col <= 22.5,
-                                          "N",
-                                          ifelse(!!dir_col > 22.5 & !!dir_col <= 67.5,
-                                                 "NE",
-                                                 ifelse(!!dir_col > 67.5 & !!dir_col <= 112.5,
-                                                        "E",
-                                                        ifelse(!!dir_col > 112.5 & !!dir_col <= 157.5,
-                                                               "SE",
-                                                               ifelse(!!dir_col > 157.5 & !!dir_col <= 202.5,
-                                                                      "S",
-                                                                      ifelse(!!dir_col > 202.5 & !!dir_col <= 247.5,
-                                                                             "SW",
-                                                                             ifelse(!!dir_col > 247.5 & !!dir_col <= 292.5,
-                                                                                    "W",
-                                                                                    ifelse(!!dir_col > 292.5 & !!dir_col <= 337.5,
-                                                                                           "NW",
-                                                                                           ifelse(!!dir_col > 337.5 & !!dir_col <= 360,
-                                                                                                  "N", ""
-                                                                                           )
-                                                                                    )
-                                                                             )
-                                                                      )
-                                                               )
-                                                        )
-                                                 )
-                                          )
-           )
-           ))
+
+    if(abbr = TRUE) { # Abbreviated response
+
+        dat <- dat %>%
+               mutate(cardinal.dir = factor(ifelse(!!dir_col >= 0 & !!dir_col <= 22.5,
+                                              "N",
+                                              ifelse(!!dir_col > 22.5 & !!dir_col <= 67.5,
+                                                     "NE",
+                                                     ifelse(!!dir_col > 67.5 & !!dir_col <= 112.5,
+                                                            "E",
+                                                            ifelse(!!dir_col > 112.5 & !!dir_col <= 157.5,
+                                                                   "SE",
+                                                                   ifelse(!!dir_col > 157.5 & !!dir_col <= 202.5,
+                                                                          "S",
+                                                                          ifelse(!!dir_col > 202.5 & !!dir_col <= 247.5,
+                                                                                 "SW",
+                                                                                 ifelse(!!dir_col > 247.5 & !!dir_col <= 292.5,
+                                                                                        "W",
+                                                                                        ifelse(!!dir_col > 292.5 & !!dir_col <= 337.5,
+                                                                                               "NW",
+                                                                                               ifelse(!!dir_col > 337.5 & !!dir_col <= 360,
+                                                                                                      "N", ""
+                                                                                               )
+                                                                                        )
+                                                                                 )
+                                                                          )
+                                                                   )
+                                                            )
+                                                     )
+                                              )
+               )
+               ))
+
+    } else { # Non-abbreviated response
+
+        dat <- dat %>%
+            mutate(cardinal.dir = factor(ifelse(!!dir_col >= 0 & !!dir_col <= 22.5,
+                                                "North",
+                                                ifelse(!!dir_col > 22.5 & !!dir_col <= 67.5,
+                                                       "North East",
+                                                       ifelse(!!dir_col > 67.5 & !!dir_col <= 112.5,
+                                                              "East",
+                                                              ifelse(!!dir_col > 112.5 & !!dir_col <= 157.5,
+                                                                     "South East",
+                                                                     ifelse(!!dir_col > 157.5 & !!dir_col <= 202.5,
+                                                                            "South",
+                                                                            ifelse(!!dir_col > 202.5 & !!dir_col <= 247.5,
+                                                                                   "South West",
+                                                                                   ifelse(!!dir_col > 247.5 & !!dir_col <= 292.5,
+                                                                                          "West",
+                                                                                          ifelse(!!dir_col > 292.5 & !!dir_col <= 337.5,
+                                                                                                 "North West",
+                                                                                                 ifelse(!!dir_col > 337.5 & !!dir_col <= 360,
+                                                                                                        "North", ""
+                                                                                                 )
+                                                                                          )
+                                                                                   )
+                                                                            )
+                                                                     )
+                                                              )
+                                                       )
+                                                )
+            )
+            ))
+
+    }
 
 return(dat)
-
 
 }
