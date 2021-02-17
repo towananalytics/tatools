e12ebe240a39e3e7f7c25818d06b052da076b777
Nick Dawe <nick@towananalytics.comgit config --global user.name Nick>
2021-02-17 23:27:00 +0800

    typos and hemisphere corrections

--- R/Season.R
+++ R/Season.R
@@ -4,48 +4,47 @@
 #'
 #' @param data a dataframe containing the data
 #' @param date_col Date variable containing the date to be used in calculating season.
-#' @param hemis Hemishere for calculating season "north" or "south"
+#' @param hemis Hemisphere for calculating season "north" or "south"
 #' @importFrom lubridate month
-#' @return the original dataframe with the season varaible included
+#' @return the original dataframe with the season variable included
 #' @examples
 #'
 #' insert_season(waves, date_time)
 #'
 #' #using dplyr
 #' library(dplyr)
-#' waves %>% insert_season
+#' waves %>% insert_season(date_time)
 #'
 #' @export
 
-insert_season <- function(data, date_col, hemis = north){
+insert_season <- function(data, date_col, hemis = south){
 
     dat <- as.data.frame(data) # Ensure data is in df format
-   # date_col <- as.Date(dat$date_col)
 
     date_col <- enquo(date_col)
-    deparse(substitute(date_col)) # for labelling
+    deparse(substitute(date_col)) # for labeling
 
-    if(tolower(deparse(substitute(hemis))) == "north") { # Northern hemisphere
+    if(tolower(deparse(substitute(hemis))) == "south") { # Southern hemisphere
 
         dat <- dat %>% mutate(Season = factor(case_when(month(!!date_col) == 1 ~ "Summer",
-                                              month(!!date_col) ==2 ~ "Summer",
-                                              month(!!date_col) == 3 ~ "Autumn",
-                                              month(!!date_col) == 4 ~ "Autumn",
-                                              month(!!date_col) == 5 ~ "Autumn",
-                                              month(!!date_col) == 6 ~ "Winter",
-                                              month(!!date_col) == 7 ~ "Winter",
-                                              month(!!date_col) == 8 ~ "Winter",
-                                              month(!!date_col) == 9 ~ "Spring",
-                                              month(!!date_col) == 10~ "Spring",
-                                              month(!!date_col) == 11 ~ "Spring",
-                                              month(!!date_col) == 12 ~ "Summer",
-                                              TRUE ~ as.character(month(!!date_col))
-                                              )
+                                                          month(!!date_col) == 2 ~ "Summer",
+                                                          month(!!date_col) == 3 ~ "Autumn",
+                                                          month(!!date_col) == 4 ~ "Autumn",
+                                                          month(!!date_col) == 5 ~ "Autumn",
+                                                          month(!!date_col) == 6 ~ "Winter",
+                                                          month(!!date_col) == 7 ~ "Winter",
+                                                          month(!!date_col) == 8 ~ "Winter",
+                                                          month(!!date_col) == 9 ~ "Spring",
+                                                          month(!!date_col) == 10~ "Spring",
+                                                          month(!!date_col) == 11 ~ "Spring",
+                                                          month(!!date_col) == 12 ~ "Summer",
+                                                          TRUE ~ as.character(month(!!date_col))
+                                                          )
                                               )
                               )
         return(dat)
 
-    } else { # Southern hemisphere
+    } else { # Northern hemisphere
 
         dat <- dat %>% mutate(Season = factor(case_when(month(!!date_col) == 1 ~ "Winter",
                                                         month(!!date_col) == 2 ~ "Winter",
